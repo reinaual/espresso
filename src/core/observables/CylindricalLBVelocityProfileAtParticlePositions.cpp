638102ab03769129913d88c8aaaa89c2707ca469
Alexander Reinauer <areinauer@icp.uni-stuttgart.de>
2023-06-24 17:55:14 +0200

    attempt in CylindricalLBVelocityProfileAtParticlePositions

--- src/core/observables/CylindricalLBVelocityProfileAtParticlePositions.cpp
+++ src/core/observables/CylindricalLBVelocityProfileAtParticlePositions.cpp
@@ -22,6 +22,8 @@
 #include "grid.hpp"
 #include "grid_based_algorithms/lb_interface.hpp"
 
+#include "communication.hpp"
+
 #include <utils/Histogram.hpp>
 #include <utils/Span.hpp>
 #include <utils/math/coordinate_transformation.hpp>
@@ -31,20 +33,37 @@
 
 namespace Observables {
 std::vector<double> CylindricalLBVelocityProfileAtParticlePositions::evaluate(
-    ParticleReferenceRange const & particles,
+    ParticleReferenceRange const &local_particles,
     const ParticleObservables::traits<Particle> &traits) const{
+  using pos_type = Utils::Vector3d;
+
+  std::vector<pos_type> local_folded_positions;
+  local_folded_positions.reserve(local_particles.size());
+
+  for (auto const &p : local_particles) {
+    local_folded_positions.emplace_back(folded_position(traits.position(p), box_geo));
+  }
+
+  std::vector<std::vector<pos_type>> global_folded_positions;
+  boost::mpi::gather(comm_cart, local_folded_positions, global_folded_positions, 0);
+  
+  if (comm_cart.rank() != 0) {
+    return {};
+  }
+
   Utils::CylindricalHistogram<double, 3> histogram(n_bins(), limits());
 
-  for (auto const &p : particles) {
-    auto const pos = folded_position(traits.position(p), box_geo);
-    auto const v = LB::get_interpolated_velocity(pos) * LB::get_lattice_speed();
+  for (auto const &vec : global_folded_positions) {
+    for (auto const &pos : vec) {
+      auto const v = LB::get_interpolated_velocity(pos) * LB::get_lattice_speed();
 
-    histogram.update(
-        Utils::transform_coordinate_cartesian_to_cylinder(
-            pos - transform_params->center(), transform_params->axis(),
-            transform_params->orientation()),
-        Utils::transform_vector_cartesian_to_cylinder(
-            v, transform_params->axis(), pos - transform_params->center()));
+      histogram.update(
+          Utils::transform_coordinate_cartesian_to_cylinder(
+              pos - transform_params->center(), transform_params->axis(),
+              transform_params->orientation()),
+          Utils::transform_vector_cartesian_to_cylinder(
+              v, transform_params->axis(), pos - transform_params->center()));
+    }
   }
 
   // normalize by number of hits per bin
