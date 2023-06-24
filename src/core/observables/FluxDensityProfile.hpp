fb12477e2c3edd77464b5303ab78fd510227cb62
Alexander Reinauer <areinauer@icp.uni-stuttgart.de>
2023-06-24 18:27:38 +0200

    attempt in FluxDensityProfile

--- src/core/observables/FluxDensityProfile.hpp
+++ src/core/observables/FluxDensityProfile.hpp
@@ -24,6 +24,8 @@
 #include "PidProfileObservable.hpp"
 #include "grid.hpp"
 
+#include "communication.hpp"
+
 #include <utils/Histogram.hpp>
 #include <utils/Span.hpp>
 
@@ -40,14 +42,32 @@ public:
   }
 
   std::vector<double>
-  evaluate(ParticleReferenceRange const & particles,
+  evaluate(ParticleReferenceRange const &local_particles,
            const ParticleObservables::traits<Particle> &traits) const override {
+    using pos_type = decltype(traits.position(std::declval<Particle>()));
+
+    std::vector<pos_type> local_folded_positions;
+    local_folded_positions.reserve(local_particles.size());
+
+    for (auto const &p : local_particles) {
+      local_folded_positions.emplace_back(folded_position(traits.position(p), box_geo));
+    }
+
+    std::vector<std::vector<pos_type>> global_folded_positions;
+    boost::mpi::gather(comm_cart, local_folded_positions, global_folded_positions, 0);
+
+    if (comm_cart.rank() != 0) {
+      return {};
+    }
+
     Utils::Histogram<double, 3> histogram(n_bins(), limits());
 
-    for (auto p : particles) {
-      auto const ppos = folded_position(traits.position(p), box_geo);
-      histogram.update(ppos, traits.velocity(p));
+    for (auto const &vec : global_folded_positions) {
+      for (auto const &p : vec) {
+        histogram.update(p);
+      }
     }
+
     histogram.normalize();
     return histogram.get_histogram();
   }
