ae3746dcb532b0dd24a819a282b73776bfb16165
Alexander Reinauer <areinauer@icp.uni-stuttgart.de>
2023-06-24 17:32:13 +0200

    attempt in LBVelocityProfile

--- src/core/observables/LBVelocityProfile.cpp
+++ src/core/observables/LBVelocityProfile.cpp
@@ -20,6 +20,8 @@
 
 #include "grid_based_algorithms/lb_interface.hpp"
 
+#include "communication.hpp"
+
 #include <utils/Histogram.hpp>
 
 #include <cstddef>
@@ -30,24 +32,28 @@
 namespace Observables {
 
 std::vector<double> LBVelocityProfile::operator()() const {
-  Utils::Histogram<double, 3> histogram(n_bins(), limits());
-  for (auto const &p : sampling_positions) {
-    const auto v = LB::get_interpolated_velocity(p) * LB::get_lattice_speed();
-    histogram.update(p, v);
-  }
-  auto hist_tmp = histogram.get_histogram();
-  auto const tot_count = histogram.get_tot_count();
-  for (std::size_t ind = 0; ind < hist_tmp.size(); ++ind) {
-    if (tot_count[ind] == 0 and not allow_empty_bins) {
-      auto const error = "Decrease sampling delta(s), bin " +
-                         std::to_string(ind) + " has no hit";
-      throw std::runtime_error(error);
+  // TODO: make a copy of LB::get_interpolated_velocity without MPI-callback and replace this one
+  if (comm_cart.rank() == 0) {
+    Utils::Histogram<double, 3> histogram(n_bins(), limits());
+    for (auto const &p : sampling_positions) {
+      const auto v = LB::get_interpolated_velocity(p) * LB::get_lattice_speed();
+      histogram.update(p, v);
     }
-    if (tot_count[ind] > 0) {
-      hist_tmp[ind] /= static_cast<double>(tot_count[ind]);
+    auto hist_tmp = histogram.get_histogram();
+    auto const tot_count = histogram.get_tot_count();
+    for (std::size_t ind = 0; ind < hist_tmp.size(); ++ind) {
+      if (tot_count[ind] == 0 and not allow_empty_bins) {
+        auto const error = "Decrease sampling delta(s), bin " +
+                          std::to_string(ind) + " has no hit";
+        throw std::runtime_error(error);
+      }
+      if (tot_count[ind] > 0) {
+        hist_tmp[ind] /= static_cast<double>(tot_count[ind]);
+      }
     }
+    return hist_tmp;
   }
-  return hist_tmp;
+  return {};
 }
 
 } // namespace Observables
